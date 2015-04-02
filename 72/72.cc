f40759d36030974a79622ddc7db7ef609ab6f810
tianmn2109 <619977462@qq.com>
2015-04-02 16:03:51 +0800

    Add problem 72: Edit Distance
     Given two words word1 and word2, find the minimum number of steps
     brequired to convert word1 to word2. (each operation is counted as 1 step.)
    
     You have the following 3 operations permitted on a word:
    
     a) Insert a character
     b) Delete a character
     c) Replace a character

---
+++ 72/72.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int> > v(m + 1, vector<int>(n + 1));  // the space must be 1 larger than the string length for 0,1....n. cases.
+        for (int i = 0; i <= m; i ++)
+            v[i][0] = i;
+
+        for (int i = 0; i <= n; i ++)
+            v[0][i] = i;
+
+        for (int i = 1; i <= m; i ++)
+            for (int j = 1; j <= n; j ++) {
+                if (word1[i - 1] == word2[j - 1])                    // the word's index should be m - 1 and n - 1,because the first character 
+                                                                     // was stored in the 1,1position.  otherwise it will be exceed.
+                    v[i][j] = v[i - 1][j - 1];
+                else
+                    v[i][j] = min(v[i -1][j], min(v[i][j - 1], v[i - 1][j - 1])) + 1;
+            }
+        return v[m][n];
+    }
+};
