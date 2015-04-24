16903071ea94bd4beb9a59c743a5d0eae82e8db1
tianmn2109 <tianmn2109ccst@163.com>
2015-04-24 14:50:37 +0800

    Add problem 198: House Robber
    You are a professional robber planning to rob houses along a street.
    Each house has a certain amount of money stashed, the only constraint
    stopping you from robbing each of them is that adjacent houses have
    security system connected and it will automatically contact the police
    if two adjacent houses were broken into on the same night.
    
    Given a list of non-negative integers representing the amount of money
    of each house, determine the maximum amount of money you can rob tonight
    without alerting the

---
+++ 199/198/198.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> v(nums.size());
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
+        v[0] = nums[0];
+        v[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i ++) {
+                v[i] = max(v[i - 2] + nums[i], v[i - 1]);
+        }
+        return max(v[nums.size() - 1], v[nums.size() - 2]);
+    }
+};
