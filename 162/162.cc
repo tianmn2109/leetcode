1fcfd5b2216bbdffedc477a9ad49921e7cb721c8
tianmn2109 <tianmn2109ccst@163.com>
2015-05-22 11:24:04 +0800

    Add problem 162: Find Peak Element
    A peak element is an element that is greater than its neighbors.
    
    Given an input array where num[i] ≠ num[i+1], find a peak element
    and return its index.
    
    The array may contain multiple peaks, in that case return the index
    to any one of the peaks is fine.
    
    You may imagine that num[-1] = num[n] = -∞.
    
    For example, in array [1, 2, 3, 1], 3 is a peak element and your
    function should return the index number 2.
    
    click to show spoilers.
    Note:
    
    Your solution should be in logarithmic complexity.

--- 162/162.cc
+++ 162/162.cc
@@ -1,31 +1,15 @@
-#include <vector>
-#include <iostream>
-using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if (nums.size() == 1)
-            return 0;
-        int left = 0;
-        int right = nums.size() - 1;
-        while (left <= right) {
-            if (left == right)
-                return left;
-            int middle = (left + right) / 2;
-            if (nums[middle] < nums[middle + 1])
-                left = middle + 1;
-            else
+       int left = 0;
+       int right = nums.size() - 1;
+       while (left < right) {
+           int middle = (left + right) / 2;;
+           if (nums[middle] > nums[middle + 1])
                 right = middle;
-        }
+            else
+                left = middle + 1;
+       }
+       return left;
     }
 };
-
-int main()
-{
-	vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	Solution s;	
-	cout << s.findPeakElement(v) << endl;
-	return 0;
-}
