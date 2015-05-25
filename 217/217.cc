50136783fb4de5a4376a1c59c8b7f64ddd19275e
tianmn2109 <tianmn2109ccst@163.com>
2015-05-25 17:28:37 +0800

    Add problem 217: Contains Duplicate

---
+++ 217/217.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s;
+        for (int i = 0; i < nums.size(); i ++)
+            if (s.count(nums[i]))
+                return true;
+            else
+                s.insert(nums[i]);
+        return false;
+    }
+};
