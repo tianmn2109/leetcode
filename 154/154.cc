8035878621a46ef3109223dab21007be053f0f8f
tianmn2109 <tianmn2109ccst@163.com>
2015-06-02 18:33:20 +0800

    Add problem 154: Find Minimum in Rotated arr II
    Follow up for "Find Minimum in Rotated Sorted Array":
    What if duplicates are allowed?
    
    Would this affect the run-time complexity? How and why?
    
    Suppose a sorted array is rotated at some pivot unknown to you beforehand.
    
    (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
    
    Find the minimum element.
    
    The array may contain duplicates.

---
+++ 154/154.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& A) {
+        if (A.size() == 0)
+            return A[0];
+        int left = 0;
+        int right = A.size() - 1;
+        while (left < right - 1) {
+            while (A[left] == A[right] && left < right)
+                right --;
+            while (right > left && A[right] == A[right - 1])
+                right --;
+            if (A[left] < A[right])
+                return A[left];
+            int middle = (left + right) / 2;
+            if (A[middle] >= A[left] && A[middle] > A[right])
+                left = middle + 1;
+            else if (A[middle] < A[right] && A[middle] < A[left])
+                right = middle;
+        }
+        return A[left] < A[right] ? A[left] : A[right];
+    }
+};
