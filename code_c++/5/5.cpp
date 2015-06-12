e071804b0a3661349661d4f285f280324dca60b8
tianmn2109 <619977462@qq.com>
2015-06-12 16:19:05 +0800

    Add other people's solutions to problem 5.

--- code_c++/5/5.cpp
+++ code_c++/5/5.cpp
@@ -44,4 +44,7 @@ public:
             
             
         }
+	return s.substr(pos, max);
+    }
+};
 
---
+++ code_c++/5/5_other_people_solutions.cc
@@ -0,0 +1,113 @@
+// 1 Brute force solution, O(N3):
+
+
+// 2 Dp O(N2) time and O(N2) space:
+
+string longestPalindromeDP(string s) {
+  int n = s.length();
+  int longestBegin = 0;
+  int maxLen = 1;
+  bool table[1000][1000] = {false};
+  for (int i = 0; i < n; i++) {
+    table[i][i] = true;
+  }
+  for (int i = 0; i < n-1; i++) {
+    if (s[i] == s[i+1]) {
+      table[i][i+1] = true;
+      longestBegin = i;
+      maxLen = 2;
+    }
+  }
+  for (int len = 3; len <= n; len++) {
+    for (int i = 0; i < n-len+1; i++) {
+      int j = i+len-1;
+      if (s[i] == s[j] && table[i+1][j-1]) {
+        table[i][j] = true;
+        longestBegin = i;
+        maxLen = len;
+      }
+    }
+  }
+  return s.substr(longestBegin, maxLen);
+}
+
+// 3 A simpler approach, O(N2) time and O(1) space:
+
+string expandAroundCenter(string s, int c1, int c2) {
+  int l = c1, r = c2;
+  int n = s.length();
+  while (l >= 0 && r <= n-1 && s[l] == s[r]) {
+    l--;
+    r++;
+  }
+  return s.substr(l+1, r-l-1);
+}
+ 
+string longestPalindromeSimple(string s) {
+  int n = s.length();
+  if (n == 0) return "";
+  string longest = s.substr(0, 1);  // a single char itself is a palindrome
+  for (int i = 0; i < n-1; i++) {
+    string p1 = expandAroundCenter(s, i, i);
+    if (p1.length() > longest.length())
+      longest = p1;
+ 
+    string p2 = expandAroundCenter(s, i, i+1);
+    if (p2.length() > longest.length())
+      longest = p2;
+  }
+  return longest;
+}
+
+
+// 4 Manacher’s algorithm
+
+// Transform S into T.
+// For example, S = "abba", T = "^#a#b#b#a#$".
+// ^ and $ signs are sentinels appended to each end to avoid bounds checking
+string preProcess(string s) {
+  int n = s.length();
+  if (n == 0) return "^$";
+  string ret = "^";
+  for (int i = 0; i < n; i++)
+    ret += "#" + s.substr(i, 1);
+ 
+  ret += "#$";
+  return ret;
+}
+ 
+string longestPalindrome(string s) {
+  string T = preProcess(s);
+  int n = T.length();
+  int *P = new int[n];
+  int C = 0, R = 0;
+  for (int i = 1; i < n-1; i++) {
+    int i_mirror = 2*C-i; // equals to i' = C - (i-C)
+    
+    P[i] = (R > i) ? min(R-i, P[i_mirror]) : 0;
+    
+    // Attempt to expand palindrome centered at i
+    while (T[i + 1 + P[i]] == T[i - 1 - P[i]])
+      P[i]++;
+ 
+    // If palindrome centered at i expand past R,
+    // adjust center based on expanded palindrome.
+    if (i + P[i] > R) {
+      C = i;
+      R = i + P[i];
+    }
+  }
+ 
+  // Find the maximum element in P.
+  int maxLen = 0;
+  int centerIndex = 0;
+  for (int i = 1; i < n-1; i++) {
+    if (P[i] > maxLen) {
+      maxLen = P[i];
+      centerIndex = i;
+    }
+  }
+  delete[] P;
+  
+  return s.substr((centerIndex - 1 - maxLen)/2, maxLen);
+}
