ab33a40c856842c334bd82cd9660da3c9387e4a9
tianmn2109 <619977462@qq.com>
2015-06-13 21:50:02 +0800

    Add other people's solution to problem 28.

---
+++ code_c++/28/other_people_solution.cc
@@ -0,0 +1,11 @@
+// 1
+int strStr(char *haystack, char *needle) {
+        if (!haystack || !needle) return -1;
+        for (int i = 0; ; ++i) {
+            for (int j = 0; ; ++j) {
+                if (needle[j] == 0) return i;
+                if (haystack[i + j] == 0) return -1;
+                if (haystack[i + j] != needle[j]) break;
+            }
+        }
+    }
