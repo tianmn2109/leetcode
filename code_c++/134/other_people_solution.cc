eb5825b47170c7196b98ca30afae9da6d6d14923
tianmn2109 <619977462@qq.com>
2015-06-25 22:24:27 +0800

    Add other people's solution to problem 134.

---
+++ code_c++/134/other_people_solution.cc
@@ -0,0 +1,20 @@
+public class Solution {
+public int canCompleteCircuit(int[] gas, int[] cost) {
+    for(int i = 0; i < gas.length; i++) {
+        gas[i] -= cost[i];
+    }
+    int sum = 0;
+    int result = 0;
+    int n = gas.length;
+    for(int i = 0; i < n * 2 - 1; i++) {
+        sum += gas[i % n];
+        if(sum < 0) {
+            result = i + 1;
+            if(result >= n) {
+                return -1;
+            }
+            sum = 0;
+        }
+    }
+    return result;
+}
