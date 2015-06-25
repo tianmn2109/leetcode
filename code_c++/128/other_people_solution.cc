67a282725433b2ddb74f25eb768b42ab903cac20
tianmn2109 <619977462@qq.com>
2015-06-25 21:27:53 +0800

    Add other people's solution to problem 128.

---
+++ code_c++/128/other_people_solution.cc
@@ -0,0 +1,13 @@
+int longestConsecutive(const vector<int> &num) {
+    unordered_set<int> s(num.begin(), num.end()), searched;
+    int longest = 0;
+    for (int i: num) {
+        if (searched.find(i) != searched.end()) continue;
+        searched.insert(i);
+        int j = i - 1, k = i + 1;
+        while (s.find(j) != s.end()) searched.insert(j--);
+        while (s.find(k) != s.end()) searched.insert(k++);
+        longest = max(longest, k - 1 - j);
+    }
+    return longest;
+}
